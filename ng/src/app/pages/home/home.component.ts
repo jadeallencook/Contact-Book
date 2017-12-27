e6582b861736b15dd5bd64c0e9d7d19c649cbb58
Jade Allen Cook <jcook@deseretnews.com>
2017-12-27 11:51:16 -0700

    ❤️ Added top 3 favorites to homepage

--- ng/src/app/pages/home/home.component.ts
+++ ng/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  favorites: Array<{}> = [];
+
+  constructor() {
+    firebase.database().ref('profiles/' + firebase.auth().currentUser.uid + '/favorites').on('value', (snapshot) => {
+      this.favorites = [];
+      if (snapshot.val()) {
+        snapshot.val().map((key) => {
+          firebase.database().ref('contacts/' + key).once('value').then((contact) => {
+            contact = contact.val();
+            let notes = Object.keys(contact.notes);
+            contact.lastNote = contact.notes[notes[notes.length - 1]].msg;
+            contact.uid = key;
+            if (this.favorites.length < 3) this.favorites.push(contact);
+          });
+        });
+      }
+    });
+  }
+
+  formatPhoneNumber(num) {
+    var s2 = ("" + num).replace(/\D/g, '');
+    var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+    return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+  }
+
 
   ngOnInit() {
   }
